946d28cf8295bf1f2c85981b67c6e74375ddd07c
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-16 15:41:13 -0800

    odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -2,7 +2,7 @@
 """
 Create a flask web application for AirBNB project
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -32,6 +32,19 @@ def python_is(text):
 @app.route("/number/<int:n>", strict_slashes=False)
 def hello_number(n):
     return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def render_number(n):
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def even_or_odd(n):
+    if n % 2 == 0:
+        return render_template("6-number_odd_or_even.html", n=n, type="even")
+    else:
+        return render_template("6-number_odd_or_even.html", n=n, type="odd")
 
 
 if __name__ == "__main__":
