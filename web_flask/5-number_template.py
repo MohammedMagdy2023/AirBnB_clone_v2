9eb9c73e9b521537c85eaa7403e31d98d627dc8a
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-16 05:53:38 -0800

    Flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,7 +34,7 @@ def hello_number(n):
     return f"{n} is a number"
 
 
-@app.route("/number_template/<n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def render_number(n):
     return render_template("5-number.html", n=n)
 
