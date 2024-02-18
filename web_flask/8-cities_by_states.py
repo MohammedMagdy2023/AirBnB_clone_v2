dd3636fd9fe1ed950e4e656155bc8655fc8edc0b
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-18 13:35:41 -0800

    Flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,10 +7,10 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_state():
     states = storage.all("State").values()
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
